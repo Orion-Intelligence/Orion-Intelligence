8fca38f85056b3ac00c4cd8cdf20ef14037859d2
عبدالمنان <msmannan00@protonmail.com>
2025-01-05 18:17:37 +0500

    swagger fixes

--- run.sh
+++ run.sh
@@ -11,8 +11,13 @@ configure_env() {
 
   if [ "$PRODUCTION" = "1" ]; then
     cp nginx/nginx-prod.conf nginx/nginx.conf
+    update_swagger_config "orion.whistleaks.com" "https"
   elif [ "$PRODUCTION" = "0" ]; then
     cp nginx/nginx-dev.conf nginx/nginx.conf
+    update_swagger_config "localhost:8080" "http"
+  else
+    echo "Invalid PRODUCTION value in .env. Defaulting to development."
+    update_swagger_config "localhost:8080" "http"
   fi
 }
 
@@ -24,9 +29,24 @@ use_compose_file() {
   fi
 }
 
+update_swagger_config() {
+  HOST=$1
+  SCHEME=$2
+  SWAGGER_FILE="swagconfig/swagconfig.json"
+
+  if [ -f "$SWAGGER_FILE" ]; then
+    echo "Updating Swagger configuration with host: $HOST and schemes: $SCHEME"
+    sed -i "s/\"host\": \".*\"/\"host\": \"$HOST\"/" "$SWAGGER_FILE"
+    sed -i "s/\"schemes\": \[.*\]/\"schemes\": [\"$SCHEME\"]/" "$SWAGGER_FILE"
+  else
+    echo "Swagger configuration file not found: $SWAGGER_FILE"
+    exit 1
+  fi
+}
+
 stop_docker
 if [ "$1" == "stop" ]; then
-    echo "crawler service stopped"
+    echo "Crawler service stopped"
 else
     configure_env
     COMMAND=$1
@@ -46,5 +66,5 @@ else
     docker compose -p $PROJECT_NAME -f $COMPOSE_FILE up -d
     docker exec -it trusted-web-main /bin/sh -c "python manage.py migrate"
 
-    echo "server started"
+    echo "Server started"
 fi
