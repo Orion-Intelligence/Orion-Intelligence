82356ce0172fd4d22f4e7fa5e21e696bee7239ca
عبدالمنان <msmannan00@protonmail.com>
2025-01-29 02:14:03 +0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,27 @@
-# Configuration file for Sphinx documentation builder.
+project = 'Orion Platform Documentation'
+copyright = '2025, Orion'
+author = 'Orion Team'
 
-# -- Project information -----------------------------------------------------
-project = 'Orion Documentation'
-author = 'Abdul Mannan'
-release = '1.0.0'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
 
-# -- General configuration ---------------------------------------------------
-extensions = ['myst_parser']
-source_suffix = ['.rst', '.md']
-master_doc = 'index'
+templates_path = ['_templates']
+exclude_patterns = []
+
 html_theme = 'sphinx_rtd_theme'
 
-# -- Options for HTML output -------------------------------------------------
+html_logo = 'https://raw.githubusercontent.com/msmannan00/Orion-Search/refs/heads/trusted-main/docs/screenshots/Logo-orion-Intelligence.png'
+
+html_theme_options = {
+    'logo_only': True,
+    'style_nav_header_background': '#004080',
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+}
+
 html_static_path = ['_static']
