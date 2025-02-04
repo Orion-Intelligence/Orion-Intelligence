9ffc7e0f51803356b5d59348f5c7d28307f816de
عبدالمنان <msmannan00@protonmail.com>
2025-02-04 22:20:36 +0500

    error page fixes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
-from routes import home, errors
-from backend.view_managers.server.error_manager.error_view_model import error_view_model
-from backend.view_managers.server.error_manager.shared_model.error_param_model import error_param_model
-
+from routes import home
 app = FastAPI()
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -14,10 +11,3 @@ app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="stat
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 app.include_router(home.router)
-app.include_router(errors.router)
-@app.exception_handler(404)
-async def custom_404_handler(request: Request, exc: HTTPException):
-    params = error_param_model(error_code=404)
-
-    response = await error_view_model.getInstance().invoke_trigger(request, params)
-    return response
--- app/routes/errors.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, HTTPException
-
-# Create a router instance
-router = APIRouter()
-
-@router.get("/nonexistent")
-def trigger_404():
-    raise HTTPException(status_code=404, detail="Page not found")
