0f76037f08e093f7d65401c98f9fe40949a1bc32
عبدالمنان <msmannan00@protonmail.com>
2025-02-12 18:48:03 +0500

    error implementations

--- app/configs/exception_handlers.py
+++ app/configs/exception_handlers.py
@@ -1,21 +1,51 @@
-from fastapi import Request
+import traceback
+import re
+from fastapi import Request, HTTPException
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import RedirectResponse, JSONResponse
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR, HTTP_422_UNPROCESSABLE_ENTITY
 
-async def global_exception_handler(request: Request, exc: Exception):
-    if request.url.path.startswith("/api"):
-        return JSONResponse(status_code=400, content={"error": str(exc)})
-    return RedirectResponse(url="/")
+from configs import config
 
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    if request.url.path.startswith("/api"):
+
+def clean_traceback(exc: Exception):
+    error_trace = traceback.format_exception(type(exc), exc, exc.__traceback__)
+    cleaned_trace = [
+        re.sub(r"\s*\^+\s*", "", line.strip())
+        for line in error_trace if line.strip()
+    ]
+    return cleaned_trace[::-1]
+
+
+async def global_exception_handler(_: Request, exc: Exception):
+    status_code = exc.status_code if isinstance(exc, HTTPException) else HTTP_500_INTERNAL_SERVER_ERROR
+
+    if config.DEBUG:
+        return JSONResponse(
+            status_code=status_code,
+            content={
+                "error": str(exc),
+                "traceback": clean_traceback(exc),
+            },
+        )
+    return RedirectResponse(url=f"/{status_code}")
+
+
+async def validation_exception_handler(_: Request, exc: RequestValidationError):
+    if config.DEBUG:
         errors = [
             {
-                "field": ".".join(str(loc) for loc in error["loc"][1:]),  # Extracts field name
+                "field": ".".join(str(loc) for loc in error["loc"][1:]),
                 "message": error["msg"],
                 "type": error["type"]
             }
             for error in exc.errors()
         ]
-        return JSONResponse(status_code=422, content={"validation_errors": errors})
-    return RedirectResponse(url="/")
+        return JSONResponse(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            content={
+                "validation_errors": errors,
+                "traceback": clean_traceback(exc),
+            },
+        )
+    return RedirectResponse(url=f"/{HTTP_422_UNPROCESSABLE_ENTITY}")
