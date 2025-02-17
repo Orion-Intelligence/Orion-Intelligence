f35d08ab4b2303fc9daf57123576cea6bae34fd9
عبدالمنان <msmannan00@protonmail.com>
2025-02-17 23:19:27 +0500

    bug fixes

--- client/src/app/pages/homapage/homepage.component.ts
+++ client/src/app/pages/homapage/homepage.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
 import {ToolbarComponent} from '../../shared/partials/toolbar/toolbar.component';
 import {HomeSearchComponent} from '../../shared/partials/home/home-search/home-search.component';
 import {HomeInsightComponent} from '../../shared/partials/home/home-insight/home-insight.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-index',
   imports: [
-    NgOptimizedImage,
     ToolbarComponent,
     HomeSearchComponent,
-    HomeInsightComponent
+    HomeInsightComponent,
+    NgOptimizedImage
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
