7cb8abe931f84815881578137ab70c2d2576fc9d
عبدالمنان <msmannan00@protonmail.com>
2025-02-26 15:34:16 +0500

    grid improvements

--- client/src/app/shared/partials/dashboard/dashboard-search-content-items/dashboard-search-content-items.component.ts
+++ client/src/app/shared/partials/dashboard/dashboard-search-content-items/dashboard-search-content-items.component.ts
@@ -1,11 +1,13 @@
-import {Component, Input} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {
-  DashboardSearchGeneralIntelligenceComponent
-} from '../dashboard-search-general-intelligence/dashboard-search-general-intelligence.component';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardSearchGeneralIntelligenceComponent } from '../dashboard-search-general-intelligence/dashboard-search-general-intelligence.component';
+import { Pages } from '../../../../constants/pages';
+import { DashboardService } from '../../../../services/dashboard/dashboard.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-search-content-items',
+  standalone: true,
   imports: [
     CommonModule,
     DashboardSearchGeneralIntelligenceComponent
@@ -13,6 +15,22 @@ import {
   templateUrl: './dashboard-search-content-items.component.html',
   styleUrl: './dashboard-search-content-items.component.css'
 })
-export class DashboardSearchContentItemsComponent {
-  @Input() selectedSection!: string;
+export class DashboardSearchContentItemsComponent implements OnInit, OnDestroy {
+  protected readonly Pages = Pages;
+  currentPage: string = '';
+  private subscription!: Subscription;
+
+  constructor(public dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.dashboardService.currentPage$.subscribe(page => {
+      this.currentPage = page;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
