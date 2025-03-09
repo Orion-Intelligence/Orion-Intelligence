5d168b145f025717466e718d244bb9314fa11372
عبدالمنان <msmannan00@protonmail.com>
2025-03-09 16:31:11 +0500

    prioirty fixes

--- backend/orion/middleware/middlewares/security_headers_middleware.py
+++ backend/orion/middleware/middlewares/security_headers_middleware.py
@@ -1,13 +1,15 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import Response
 from configs import config
+from orion.helper_manager.env_handler import env_handler
 
 
 class security_headers_middleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response: Response = await call_next(request)
+        DEBUG = env_handler.get_instance().env("PRODUCTION", "0") != "1"
 
-        if not config.DEBUG:
+        if DEBUG:
             hsts_value = f"max-age={config.SECURE_HSTS_SECONDS}; includeSubDomains; preload"
             response.headers["Strict-Transport-Security"] = hsts_value
         else:
