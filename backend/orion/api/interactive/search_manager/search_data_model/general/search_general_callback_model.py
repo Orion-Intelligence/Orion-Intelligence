b68152c318dc20427f6042fb111c611c00576459
عبدالمنان <msmannan00@protonmail.com>
2025-03-11 03:55:23 +0500

    model fixes

--- backend/orion/api/interactive/search_manager/search_data_model/general/search_general_callback_model.py
+++ backend/orion/api/interactive/search_manager/search_data_model/general/search_general_callback_model.py
@@ -10,18 +10,18 @@ class suggestion(BaseModel):
 
 class result_item(BaseModel):
     m_title: Optional[str] = None
-    m_url: Optional[HttpUrl] = None
-    m_base_url: Optional[HttpUrl] = None
+    m_url: Optional[str] = None
+    m_base_url: Optional[str] = None
     m_content: Optional[str] = None
     m_important_content: Optional[str] = None
     m_network: Optional[str] = None
     m_meta_description: Optional[str] = None
-    m_weblink: Optional[List[HttpUrl]] = None
-    m_dumplink: Optional[List[HttpUrl]] = None
-    m_sub_url: Optional[List[HttpUrl]] = None
+    m_weblink: Optional[List[str]] = None
+    m_dumplink: Optional[List[str]] = None
+    m_sub_url: Optional[List[str]] = None
     m_document: Optional[List[str]] = None
     m_video: Optional[List[str]] = None
-    m_archive_url: Optional[List[HttpUrl]] = None
+    m_archive_url: Optional[List[str]] = None
     m_images: Optional[List[str]] = None
     m_validity_score: Optional[int] = None
     m_meta_keywords: Optional[str] = None
@@ -30,13 +30,13 @@ class result_item(BaseModel):
     m_names: Optional[List[str]] = None
     m_emails: Optional[List[str]] = None
     m_phone_numbers: Optional[List[str]] = None
-    m_clearnet_links: Optional[List[HttpUrl]] = None
-    m_update_date: Optional[datetime] = None
+    m_clearnet_links: Optional[List[str]] = None
+    m_update_date: Optional[str] = None
     m_hash_content: Optional[str] = None
     m_hash_url: Optional[str] = None
     m_hash: Optional[str] = None
-    m_creation_date: Optional[datetime] = None
-    m_contact_link: Optional[HttpUrl] = None
+    m_creation_date: Optional[str] = None
+    m_contact_link: Optional[str] = None
 
 class search_general_callback_model(BaseModel):
     Result: Optional[List[result_item]] = None
--- backend/orion/api/interactive/search_manager/search_data_model/leak/search_leak_callback_model.py
+++ backend/orion/api/interactive/search_manager/search_data_model/leak/search_leak_callback_model.py
@@ -10,14 +10,14 @@ class suggestion(BaseModel):
 
 class result_item(BaseModel):
     m_title: Optional[str] = None
-    m_url: Optional[HttpUrl] = None
-    m_base_url: Optional[HttpUrl] = None
+    m_url: Optional[str] = None
+    m_base_url: Optional[str] = None
     m_content: Optional[str] = None
     m_important_content: Optional[str] = None
     m_network: Optional[str] = None
     m_content_type: Optional[Union[str, List[str]]] = None
-    m_weblink: Optional[List[HttpUrl]] = None
-    m_dumplink: Optional[List[HttpUrl]] = None
+    m_weblink: Optional[List[str]] = None
+    m_dumplink: Optional[List[str]] = None
     m_name: Optional[str] = None
     m_email_addresses: Optional[List[str]] = None
     m_industry: Optional[str] = None
@@ -27,14 +27,14 @@ class result_item(BaseModel):
     m_websites: Optional[List[str]] = None
     m_company_name: Optional[str] = None
     m_logo_or_images: Optional[List[str]] = None
-    m_leak_date: Optional[datetime] = None
+    m_leak_date: Optional[str] = None
     m_data_size: Optional[str] = None
     m_country_name: Optional[str] = None
     m_revenue: Optional[str] = None
     m_hash: Optional[str] = None
-    m_update_date: Optional[datetime] = None
-    m_contact_link: Optional[HttpUrl] = None
-    m_creation_date: Optional[datetime] = None
+    m_update_date: Optional[str] = None
+    m_contact_link: Optional[str] = None
+    m_creation_date: Optional[str] = None
 
 class search_leak_callback_model(BaseModel):
     Result: Optional[List[result_item]] = None
