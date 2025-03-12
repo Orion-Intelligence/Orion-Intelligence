f0dd67435d3f8147166ca0e176b1cfdf6449324a
عبدالمنان <msmannan00@protonmail.com>
2025-03-12 13:40:06 +0500

    scroll fixes

--- client/src/app/shared/partials/intel-results/intel-panel/dashboard-results/dashboard-leak-result-grid/dashboard-leak-result-grid.component.ts
+++ client/src/app/shared/partials/intel-results/intel-panel/dashboard-results/dashboard-leak-result-grid/dashboard-leak-result-grid.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, AfterViewChecked, Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import {NgForOf} from '@angular/common';
 import {DashboardService} from '../../../../../../services/dashboard/dashboard.service';
 import {Suggestion} from '../../../../../model/intel-results/general/search_general_callback_model';
@@ -9,11 +9,10 @@ import {Router, ActivatedRoute, RouterLink} from '@angular/router';
   imports: [NgForOf, RouterLink],
   templateUrl: './dashboard-leak-result-grid.component.html',
 })
-export class DashboardLeakResultGridComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class DashboardLeakResultGridComponent implements OnInit, AfterViewInit {
   @Input() suggestion!: Suggestion | undefined;
   currentUrl: string = '';
   queryParams: any = {};
-  private hasScrolled = false; // To prevent multiple scrolls
 
   constructor(public dashboardService: DashboardService, private router: Router, private route: ActivatedRoute) {}
 
@@ -26,13 +25,6 @@ export class DashboardLeakResultGridComponent implements OnInit, AfterViewInit, 
 
   ngAfterViewInit() {
     this.scrollToSavedItem();
-  }
-
-  ngAfterViewChecked() {
-    if (!this.hasScrolled) {
-      this.scrollToSavedItem();
-      this.hasScrolled = true;
-    }
   }
 
   saveSession(itemId: string) {
@@ -49,5 +41,4 @@ export class DashboardLeakResultGridComponent implements OnInit, AfterViewInit, 
         element.scrollIntoView();
       }
     }
-  }
-}
+  }}
