dd5841d042e6d1c03f1606bb2d02c5eb05c3412f
alphadeveloper12 <64380267+alphadeveloper12@users.noreply.github.com>
2025-03-13 17:35:40 +0500

    defacement code added

--- client/src/app/shared/partials/intel-results/intel-panel/dashboard-results/dashboard-defacement-result-grid/dashboard-defacement-result-grid.component.ts
+++ client/src/app/shared/partials/intel-results/intel-panel/dashboard-results/dashboard-defacement-result-grid/dashboard-defacement-result-grid.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import { Observable } from 'rxjs';
+import { Defacement_callback_model } from '../../../../../model/intel-results/defacement/defacement_callback_model';
+import { DefacementService } from '../../../../../services/defacement.service';
 
 @Component({
   selector: 'app-dashboard-defacement-result-grid',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, NgForOf, AsyncPipe],
   templateUrl: './dashboard-defacement-result-grid.component.html',
-  styleUrl: './dashboard-defacement-result-grid.component.css'
+  styleUrls: ['./dashboard-defacement-result-grid.component.css']
 })
-export class DashboardDefacementResultGridComponent {
+export class DashboardDefacementResultGridComponent implements OnInit {
+  resultData$!: Observable<Defacement_callback_model[]>;
 
+  constructor(private resultDataService: DefacementService) {}
+
+  ngOnInit(): void {
+    this.resultData$ = this.resultDataService.getResults();
+  }
 }
