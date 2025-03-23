eae50efc1df72ee89de428a23898cc69a8089c94
عبدالمنان <msmannan00@protonmail.com>
2025-03-23 15:02:28 +0500

    ui fixes

--- client/src/app/shared/partials/empty-result/empty-result.component.ts
+++ client/src/app/shared/partials/empty-result/empty-result.component.ts
@@ -1,13 +1,17 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, SimpleChanges} from '@angular/core';
 import {Category} from '../../enums/pages';
 
 @Component({
-  selector: 'app-empty-result',
-  imports: [],
-  templateUrl: './empty-result.component.html',
+  selector: 'app-empty-result', imports: [], templateUrl: './empty-result.component.html',
 })
 export class EmptyResultComponent {
   @Input() searchQuery!: string;
+  query = ""
+
+  ngOnInit(_: SimpleChanges): void {
+    this.query = this.searchQuery
+    alert(this.query)
+  }
 
   constructor() {
   }
