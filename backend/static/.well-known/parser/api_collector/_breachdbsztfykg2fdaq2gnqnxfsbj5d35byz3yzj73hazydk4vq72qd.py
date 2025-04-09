65de2d6fb7cf1e0aaae53fec48205e3f5d796757
عبدالمنان <msmannan00@protonmail.com>
2025-04-09 17:00:50 +0500

    script fixes

--- backend/static/.well-known/parser/api_collector/_breachdbsztfykg2fdaq2gnqnxfsbj5d35byz3yzj73hazydk4vq72qd.py
+++ backend/static/.well-known/parser/api_collector/_breachdbsztfykg2fdaq2gnqnxfsbj5d35byz3yzj73hazydk4vq72qd.py
@@ -30,7 +30,7 @@ class _breachdbsztfykg2fdaq2gnqnxfsbj5d35byz3yzj73hazydk4vq72qd(api_collector_in
 
   @property
   def rule_config(self) -> RuleModel:
-    return RuleModel(m_fetch_proxy=FetchProxy.TOR, m_fetch_config=FetchConfig.SELENIUM, m_threat_type=ThreatType.API)
+    return RuleModel(m_fetch_proxy=FetchProxy.TOR, m_fetch_config=FetchConfig.PLAYRIGHT, m_threat_type=ThreatType.API)
 
   @property
   def card_data(self) -> List[leak_model]:
