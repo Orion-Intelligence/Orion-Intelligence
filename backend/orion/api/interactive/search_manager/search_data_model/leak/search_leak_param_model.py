9d567f2d96a608c72b6240f94557e17480a7cbcb
عبدالمنان <msmannan00@protonmail.com>
2025-05-26 18:13:36 +0500

    Update search_leak_param_model.py

--- backend/orion/api/interactive/search_manager/search_data_model/leak/search_leak_param_model.py
+++ backend/orion/api/interactive/search_manager/search_data_model/leak/search_leak_param_model.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 class search_leak_param_model(BaseModel):
     q: Optional[str] = Field("", max_length=150)
-    pSearchParamType: Optional[str] = "all"
+    mSearchParamType: Optional[str] = "all"
     mSearchParamPage: Optional[int] = 1
     mSearchParamSafeSearch: bool = False
     mNetwork: str = "all"
