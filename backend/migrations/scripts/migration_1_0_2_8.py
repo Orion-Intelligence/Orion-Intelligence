e3768f4b02a7800ae311f89ecb7115c78cd52e71
عبدالمنان <msmannan00@protonmail.com>
2025-07-22 03:00:26 +0500

    Update migration_1_0_2_8.py

--- backend/migrations/scripts/migration_1_0_2_8.py
+++ backend/migrations/scripts/migration_1_0_2_8.py
@@ -41,7 +41,7 @@ class migration_1_0_2_8:
         es = elastic.get_connection()
 
         scroll = "15m"
-        size = 10000
+        size = 5000
         total_indexed = 0
         total_skipped = 0
         batch_count = 0
