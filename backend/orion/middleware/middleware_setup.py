dabe1c81cf48b0cc179ad5a3c13ac15ff22d400d
عبدالمنان <msmannan00@protonmail.com>
2025-09-30 16:54:37 +0500

    Update middleware_setup.py

--- backend/orion/middleware/middleware_setup.py
+++ backend/orion/middleware/middleware_setup.py
@@ -10,23 +10,23 @@ from orion.middleware.middlewares.service_ready_middleware import service_ready_
 
 
 def setup_middlewares(app):
-    # app.add_middleware(content_security_policy_middleware)
-    # app.add_middleware(service_ready_middleware)
+    app.add_middleware(content_security_policy_middleware)
+    app.add_middleware(service_ready_middleware)
     PRODUCTION_DOMAIN = env_handler.get_instance().env("PRODUCTION_DOMAIN", "-")
 
-    # app.add_middleware(
-    #     CORSMiddleware,
-    #     allow_origins=PRODUCTION_DOMAIN,
-    #     allow_credentials=True,
-    #     allow_methods=["GET", "POST", "PUT", "DELETE"],
-    #     allow_headers=["Authorization", "Content-Type"]
-    # )
-    #
-    # if not config.DEBUG:
-    #     app.add_middleware(
-    #         TrustedHostMiddleware,
-    #         allowed_hosts=PRODUCTION_DOMAIN
-    #     )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=PRODUCTION_DOMAIN,
+        allow_credentials=True,
+        allow_methods=["GET", "POST", "PUT", "DELETE"],
+        allow_headers=["Authorization", "Content-Type"]
+    )
 
-    # app.add_middleware(security_headers_middleware)
-    # app.add_middleware(content_block_middleware)
+    if not config.DEBUG:
+        app.add_middleware(
+            TrustedHostMiddleware,
+            allowed_hosts=PRODUCTION_DOMAIN
+        )
+
+    app.add_middleware(security_headers_middleware)
+    app.add_middleware(content_block_middleware)
