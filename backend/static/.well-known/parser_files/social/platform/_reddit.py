33e7b8106b163c3ebaa5797eb7053beccbe48b76
عبدالمنان <msmannan00@protonmail.com>
2025-10-05 03:01:58 +0500

    fixes

--- backend/static/.well-known/parser_files/social/platform/_reddit.py
+++ backend/static/.well-known/parser_files/social/platform/_reddit.py
@@ -54,7 +54,7 @@ class _reddit(leak_extractor_interface, ABC):
     @property
     def rule_config(self) -> RuleModel:
         return RuleModel(
-            m_fetch_proxy=FetchProxy.NONE,
+            m_fetch_proxy=FetchProxy.TOR,
             m_fetch_config=FetchConfig.PLAYRIGHT,
             m_threat_type=ThreatType.REDDIT
         )
