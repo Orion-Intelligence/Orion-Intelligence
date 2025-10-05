f91fe5be03a854552d3128d794f299969802c9e6
عبدالمنان <msmannan00@protonmail.com>
2025-10-06 03:52:25 +0500

    Update payment_gateway_middleware.py

--- backend/orion/middleware/middlewares/payment_gateway_middleware.py
+++ backend/orion/middleware/middlewares/payment_gateway_middleware.py
@@ -1,10 +1,10 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from orion.services.session_manager.session_manager import session_manager
 
-TRIAL_DAYS = 14 
+TRIAL_DAYS = 14
 TRIAL_END_URL = "/paymentGateway"
 
 
@@ -16,7 +16,7 @@ class payment_gateway_middleware(BaseHTTPMiddleware):
         path = request.url.path
         if path.startswith("/api/") or path.startswith("/login"):
             return await call_next(request)
-        
+
         if not (path == "/dashboard" or path.startswith("/dashboard/")):
             return await call_next(request)
         auth_header = request.headers.get("Authorization", "")
@@ -26,6 +26,7 @@ class payment_gateway_middleware(BaseHTTPMiddleware):
 
         if not token:
             return RedirectResponse(url="/login", status_code=302)
+
         try:
             user = await session_manager.get_instance().get_current_user(token)
             if not user:
@@ -37,23 +38,18 @@ class payment_gateway_middleware(BaseHTTPMiddleware):
             if not verify_date_str:
                 return RedirectResponse(url=TRIAL_END_URL, status_code=302)
 
-            account_verify_at = datetime.fromisoformat(verify_date_str)
+            account_verify_at = datetime.fromisoformat(str(verify_date_str))
+            if account_verify_at.tzinfo is None:
+                account_verify_at = account_verify_at.replace(tzinfo=timezone.utc)
+
             expire_date = account_verify_at + timedelta(days=TRIAL_DAYS)
-            now = datetime.now()
-            if subscription_status==False:  
+            now = datetime.now(timezone.utc)
+
+            if not subscription_status:
                 if now > expire_date:
-                    return RedirectResponse(
-                        url=f"{TRIAL_END_URL}?expired=1", status_code=302
-                    )
-                else:
-                    remaining_days = (expire_date - now).days
-                    return RedirectResponse(
-                        url=f"/dashboard?trial_days_left={remaining_days}",
-                        status_code=302,
-                    )
+                    return RedirectResponse(url=f"{TRIAL_END_URL}?expired=1", status_code=302)
 
         except Exception as e:
-            print("Payment gateway middleware error:", e)
             return RedirectResponse(url="/login", status_code=302)
 
         response = await call_next(request)
