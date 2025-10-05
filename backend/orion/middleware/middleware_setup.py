84c428bf44edb1e55a3c1b70c03223e34e772d92
عبدالمنان <msmannan00@protonmail.com>
2025-10-06 04:00:51 +0500

    payment fixes

--- backend/orion/middleware/middleware_setup.py
+++ backend/orion/middleware/middleware_setup.py
@@ -8,13 +8,11 @@ from orion.middleware.middlewares.content_block_middleware import content_block_
 from orion.middleware.middlewares.content_security_policy_middleware import content_security_policy_middleware
 from orion.middleware.middlewares.security_headers_middleware import security_headers_middleware
 from orion.middleware.middlewares.service_ready_middleware import service_ready_middleware
-from orion.middleware.middlewares.payment_gateway_middleware import payment_gateway_middleware
 
 
 def setup_middlewares(app):
     app.add_middleware(content_security_policy_middleware)
     app.add_middleware(service_ready_middleware)
-    app.add_middleware(payment_gateway_middleware)
     PRODUCTION_DOMAIN = env_handler.get_instance().env("PRODUCTION_DOMAIN", "-")
 
     app.add_middleware(
