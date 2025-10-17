75a4b569321f611aadfde5b3c96212a518ceb9de
MUHAMMADALI706 <105616718+MUHAMMADALI706@users.noreply.github.com>
2025-10-17 23:14:58 +0500

    implement consolidated scan and live api

--- client/src/app/shared/partials/intel-panel/dashboard-managers/dashboard-consolidated/dashboard-consolidated.component.ts
+++ client/src/app/shared/partials/intel-panel/dashboard-managers/dashboard-consolidated/dashboard-consolidated.component.ts
@@ -1,30 +1,39 @@
-import {AfterViewInit, ChangeDetectorRef, Component, OnInit, signal} from '@angular/core';
-import {AppService} from '../../../../../services/core/app/app.service';
-import {DashboardService} from '../../../../../services/dashboard/dashboard.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {combineLatest, distinctUntilChanged, map, switchMap, timer} from 'rxjs';
-import {fadeInDashboardItem} from '../../../../animations/dashboard.item.animation';
-import {NgForOf, NgIf, TitleCasePipe} from '@angular/common';
-import {ResultComponent} from '../../../result/result.component';
-import {DashboardResultsGeneralGridComponent} from '../../dashboard-results/dashboard-results-general-grid/dashboard-results-general-grid.component';
-import {ConsolidatedCallbackModel} from '../../../../model/results/consolidated/consolidated.callback.model';
-import {DashboardResultExploitComponent} from '../../dashboard-results/dashboard-result-exploit/dashboard-result-exploit.component';
-import {DashboardResultChatComponent} from '../../dashboard-results/dashboard-result-chat/dashboard-result-chat.component';
-import {SortGroupedResultsPipe} from '../../../../pipes/sort-grouped-results.pipe';
-import {ApiSubCategory, BreachSubCategory, Category, DefacementSubCategory, DumpSubCategory, FeedSubCategory, GeneralSubCategory, SocialSubCategory} from '../../../../constants/pages';
-import {SelectionStoreService} from '../../../../../services/dashboard/selection.service';
-import {TooltipDirective} from '../../../../directive/tooltip-directive.directive';
-import {DashboardResultSocialComponent} from '../../dashboard-results/dashboard-result-social/dashboard-result-social.component';
-import {ResultInsightsComponent} from "../result-insights/result-insights.component";
-import {consolidated_filters} from '../../../../constants/filters';
-import {ALLOWED_CONSOLIDATED_RANKED_SINGLETON} from '../../../../constants/shared-enums';
-import {ThreatResultsComponent} from "./defacement-results/threat-results.component";
-import {RankedCallbackModel} from '../../../../model/results/consolidated/ranked.callback.model';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
+import { AppService } from '../../../../../services/core/app/app.service';
+import { DashboardService } from '../../../../../services/dashboard/dashboard.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest, distinctUntilChanged, map, switchMap, timer } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { fadeInDashboardItem } from '../../../../animations/dashboard.item.animation';
+import { NgClass, NgForOf, NgIf, NgSwitch, NgSwitchCase, TitleCasePipe } from '@angular/common';
+import { ResultComponent } from '../../../result/result.component';
+import { DashboardResultsGeneralGridComponent } from '../../dashboard-results/dashboard-results-general-grid/dashboard-results-general-grid.component';
+import { ConsolidatedCallbackModel } from '../../../../model/results/consolidated/consolidated.callback.model';
+import { DashboardResultExploitComponent } from '../../dashboard-results/dashboard-result-exploit/dashboard-result-exploit.component';
+import { DashboardResultChatComponent } from '../../dashboard-results/dashboard-result-chat/dashboard-result-chat.component';
+import { SortGroupedResultsPipe } from '../../../../pipes/sort-grouped-results.pipe';
+import { ApiSubCategory, BreachSubCategory, Category, DefacementSubCategory, DumpSubCategory, FeedSubCategory, GeneralSubCategory, SocialSubCategory } from '../../../../constants/pages';
+import { SelectionStoreService } from '../../../../../services/dashboard/selection.service';
+import { TooltipDirective } from '../../../../directive/tooltip-directive.directive';
+import { DashboardResultSocialComponent } from '../../dashboard-results/dashboard-result-social/dashboard-result-social.component';
+import { ResultInsightsComponent } from "../result-insights/result-insights.component";
+import { consolidated_filters } from '../../../../constants/filters';
+import { ALLOWED_CONSOLIDATED_RANKED_SINGLETON } from '../../../../constants/shared-enums';
+import { ThreatResultsComponent } from "./defacement-results/threat-results.component";
+import { RankedCallbackModel } from '../../../../model/results/consolidated/ranked.callback.model';
+import { ApiService } from '../../../../services/api.service';
+import { HelperService } from '../../../../services/helper.service';
+import { HttpClient } from '@angular/common/http';
+import { ConsolidatedLiveApis } from '../../../../model/results/consolidated/consolidated.callback.model';
+import { ConsolidatedLiveApiResults } from '../../../../model/results/consolidated/consolidated.callback.model';
+import { ConsolidatedApiService } from '../../../../services/consolidated.api.service';
+import { ConsolidatedScanResults } from '../../../../model/results/consolidated/consolidated.callback.model';
+
 
 @Component({
   selector: 'app-dashboard-consolidated',
   standalone: true,
-  imports: [NgIf, ResultComponent, DashboardResultsGeneralGridComponent, NgForOf, TitleCasePipe, DashboardResultExploitComponent, DashboardResultChatComponent, SortGroupedResultsPipe, TooltipDirective, DashboardResultSocialComponent, ResultInsightsComponent, ThreatResultsComponent],
+  imports: [NgClass, NgIf, NgSwitch, NgSwitchCase, ResultComponent, DashboardResultsGeneralGridComponent, NgForOf, TitleCasePipe, DashboardResultExploitComponent, DashboardResultChatComponent, SortGroupedResultsPipe, TooltipDirective, DashboardResultSocialComponent, ResultInsightsComponent, ThreatResultsComponent],
   templateUrl: './dashboard-consolidated.component.html',
   styleUrl: './dashboard-consolidated.component.css',
   animations: [fadeInDashboardItem]
@@ -53,9 +62,23 @@ export class DashboardConsolidatedComponent implements OnInit, AfterViewInit {
   generalCategories = Object.values(GeneralSubCategory);
   leakCategories = Object.values(BreachSubCategory);
   defacementCategories = Object.values(DefacementSubCategory);
-  rankedResult:RankedCallbackModel = new RankedCallbackModel();
+  rankedResult: RankedCallbackModel = new RankedCallbackModel();
 
-  constructor(public appService: AppService, public dashboardService: DashboardService, private router: Router, private route: ActivatedRoute, private cdr: ChangeDetectorRef, protected selectionStore: SelectionStoreService) {
+  scandomains: string[] = [];
+  scanEmails: string[] = [];
+  scanUsers: string[] = [];
+  scanPlaystoreUrls: string[] = [];
+
+  scanResults: ConsolidatedScanResults[] = [];
+  isScanLoading = false;
+  private scanTriggered = false;
+
+  liveApiEntities: ConsolidatedLiveApis[] = [];
+
+  liveApiResults: ConsolidatedLiveApiResults[] = [];
+  isProcessing = false;
+
+  constructor(public http: HttpClient, public appService: AppService, public dashboardService: DashboardService, private router: Router, private route: ActivatedRoute, private cdr: ChangeDetectorRef, protected selectionStore: SelectionStoreService, private apiService: ApiService, private helperService: HelperService, private liveApiService: ConsolidatedApiService) {
     this.pageCounts = {};
   }
 
@@ -90,6 +113,21 @@ export class DashboardConsolidatedComponent implements OnInit, AfterViewInit {
 
       this.firstTrigger = false;
     });
+
+    this.initEntities();
+    if (this.liveApiEntities && this.liveApiEntities.length > 0) {
+      this.LiveApisSearch();
+    }
+
+    if (this.scandomains.length === 0) {
+      return;
+    }
+    if (this.scanTriggered) {
+      return;
+    }
+    this.scanTriggered = true;
+    this.runDomainScan();
+
   }
 
   fetchSearchResults(_ = false): void {
@@ -106,7 +144,7 @@ export class DashboardConsolidatedComponent implements OnInit, AfterViewInit {
     if (!this.dashboardService.consolidatedParamModel.q) {
       this.isLoading.set(false);
       this.dashboardService.consolidatedParamModel.q = '';
-      this.router.navigate([], {queryParams: {}, queryParamsHandling: ''}).then();
+      this.router.navigate([], { queryParams: {}, queryParamsHandling: '' }).then();
     }
 
     this.isLoading.set(true);
@@ -257,7 +295,7 @@ export class DashboardConsolidatedComponent implements OnInit, AfterViewInit {
     }
     const routePrefix = '/dashboard/' + section.toLowerCase() + '/' + second_category;
     this.router.navigate([routePrefix], {
-      queryParams: {page: 1}, queryParamsHandling: 'merge'
+      queryParams: { page: 1 }, queryParamsHandling: 'merge'
     }).then();
   }
 
@@ -313,4 +351,99 @@ export class DashboardConsolidatedComponent implements OnInit, AfterViewInit {
   }
 
 
+  initEntities() {
+    const filters = this.appService.configData().localSettings.entityfilterCategories;
+    const queryDomains = this.helperService.extractLinks('') || [];
+    const filterDomains = Array.isArray(filters['m_domain'])
+      ? filters['m_domain'].map((domain: string) => `https://${domain}`)
+      : [];
+    this.scandomains = Array.from(new Set([...queryDomains, ...filterDomains]));
+    const unique = (key: string): string[] =>
+      Array.isArray(filters[key]) ? Array.from(new Set(filters[key])) : [];
+    const scanEmails = unique('m_email');
+    const scanUsers = unique('m_social_media_profiles');
+    const scanPlaystoreUrls = unique('m_url').filter(url =>
+      url.includes('play.google.com/store/apps')
+    );
+    this.liveApiEntities = [
+      ...scanEmails.map(email => ({
+        type: 'user' as const,
+        q1: '',
+        q2: email
+      })),
+      ...scanUsers.map(userHandle => ({
+        type: 'social' as const,
+        q1: userHandle
+      })),
+      ...scanPlaystoreUrls.map(url => ({
+        type: 'cracked' as const,
+        q1: url
+      }))
+    ];
+  }
+
+  runDomainScan() {
+    this.isScanLoading = true;
+    this.scanResults = [];
+    this.liveApiService.runScanDomains(this.scandomains)
+      .pipe(
+        finalize(() => {
+          this.isScanLoading = false;
+        })
+      )
+      .subscribe({
+        next: (results: ConsolidatedScanResults[]) => {
+          this.scanResults = results;
+        },
+        error: (err) => {
+          this.isScanLoading = false;
+        }
+      });
+  }
+  extractHost(url: string): string {
+    try {
+      return new URL(url).hostname;
+    } catch {
+      return url;
+    }
+  }
+
+  getGradeClass(grade: string): string {
+    if (['D', 'F'].includes(grade)) return 'scan_report-section-danger';
+    if (grade === 'C') return 'scan_report-section-warning';
+    return '';
+  }
+  getScanResult(domain: string) {
+    return this.scanResults.find(r => r.domain === domain) || null;
+  }
+
+
+  LiveApisSearch(): void {
+    if (!this.liveApiEntities || this.liveApiEntities.length === 0) {
+      return;
+    }
+
+    this.isProcessing = true;
+    this.liveApiResults = this.liveApiEntities.map(input => ({
+      input,
+      status: 'pending',
+      resultData: null,
+      errorMessage: null,
+    }));
+
+    this.liveApiService.runLiveApiSearch(this.liveApiEntities)
+      .pipe(
+        finalize(() => {
+          this.isProcessing = false;
+        })
+      )
+      .subscribe({
+        next: (finalResults: ConsolidatedLiveApiResults[]) => {
+          this.liveApiResults = finalResults;
+        },
+        error: (err) => {
+          this.isProcessing = false;
+        }
+      });
+  }
 }
