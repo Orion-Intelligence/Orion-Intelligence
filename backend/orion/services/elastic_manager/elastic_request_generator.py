8470daadac2d59e2533443d47c52046100e86eb9
عبدالمنان <msmannan00@protonmail.com>
2025-10-19 04:13:18 +0500

    fix

--- backend/orion/services/elastic_manager/elastic_request_generator.py
+++ backend/orion/services/elastic_manager/elastic_request_generator.py
@@ -1542,7 +1542,7 @@ class elastic_request_generator:
                         "knn": {
                             "field": ELASTIC_SEMANTIC.S_EMBED_FIELD,
                             "k": CONSTANTS.S_SETTINGS_FETCHED_DOCUMENT_SIZE,
-                            "num_candidates": 50000,
+                            "num_candidates": 100,
                             "query_vector": qvec
                         }
                     }
