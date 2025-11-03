c714114f48cf69ea539b532ab7185272d7a8aec5
عبدالمنان <msmannan00@protonmail.com>
2025-11-03 16:41:48 +0500

    Update elastic_request_generator.py

--- backend/orion/services/elastic_manager/elastic_request_generator.py
+++ backend/orion/services/elastic_manager/elastic_request_generator.py
@@ -1155,11 +1155,9 @@ class elastic_request_generator:
         user = helper_controller.extract_first_email(p_query_model.q)
 
         if not p_query_model.user and user:
-            print("::::::::::::::::::::::", flush=True)
             p_query_model.user = user
 
         if not p_query_model.url and not p_query_model.user and consolidated:
-            print("::::::::::::::::::::::", flush=True)
             return None, None
 
         user_query = p_query_model.user.strip() if p_query_model.user and p_query_model.user != "*" else ""
@@ -1282,10 +1280,6 @@ class elastic_request_generator:
 
         if not (user_query or url_query or extra_user_terms or extra_domains):
             query["sort"] = ["_doc"]
-
-        print("::::::::::::::::::::::3", flush=True)
-        print(query, flush=True)
-        print("::::::::::::::::::::::3", flush=True)
 
         return ELASTIC_INDEX.S_STEALERLOGS_INDEX, query
 
