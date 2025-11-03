33008be5d4e5305a635f89c424bbf3cbce6ea33c
عبدالمنان <msmannan00@protonmail.com>
2025-11-03 23:11:47 +0500

    fixes

--- backend/orion/services/elastic_manager/elastic_controller.py
+++ backend/orion/services/elastic_manager/elastic_controller.py
@@ -389,6 +389,7 @@ class elastic_controller:
                             if idx:
                                 target_indices.add(idx)
             response = await self.__m_dump_connection.bulk(body=p_data, request_timeout=220)
+            print("pppp ::::::::::::::::::::::::::::: " + str(response))
             return response
         except Exception as ex:
             log.g().e(f"{MANAGE_ELASTIC_MESSAGES.S_INSERT_FAILURE} : {str(ex)}")
--- backend/orion/services/elastic_manager/elastic_request_generator.py
+++ backend/orion/services/elastic_manager/elastic_request_generator.py
@@ -1508,10 +1508,7 @@ class elastic_request_generator:
 
             if log["type"] == 'c' or log["type"] == 'credential':
                 if not email and not username:
-                    print("xxxx ::::::::::::::::::::::::::::: ")
                     continue
-                else:
-                    print("zzzz ::::::::::::::::::::::::::::: ")
 
                 val = email or username
                 seed = str(val) + "|" + str(channel or "")
