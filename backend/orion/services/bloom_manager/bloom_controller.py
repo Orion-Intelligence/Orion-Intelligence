517da05209ca0e89ac3a2b8593a5a6982d8d3978
عبدالمنان <msmannan00@protonmail.com>
2025-11-03 14:19:22 +0500

    Update bloom_controller.py

--- backend/orion/services/bloom_manager/bloom_controller.py
+++ backend/orion/services/bloom_manager/bloom_controller.py
@@ -174,7 +174,7 @@ class bloom_controller:
         return False
 
     def isduplicate(self,text):
-        data=self._to_bytes(text + "1")
+        data=self._to_bytes(text)
         for L in reversed(self.layers):
             if self._contains_layer_blocked(L,data): return True
         L=self.layers[-1]; self._add_layer_blocked(L,data)
