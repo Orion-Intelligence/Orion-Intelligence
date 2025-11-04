ff04d31e26ce655aee2b3ffe3b9b39582675a3ea
عبدالمنان <msmannan00@protonmail.com>
2025-11-04 13:05:15 +0500

    Update elastic_request_generator.py

--- backend/orion/services/elastic_manager/elastic_request_generator.py
+++ backend/orion/services/elastic_manager/elastic_request_generator.py
@@ -1524,7 +1524,10 @@ class elastic_request_generator:
             _id = str(datetime.utcnow().year) + "_UTC_" + m_hash
 
             if bf.isduplicate(m_hash):
+                print("pppp ::::::::::::::::::::::::::::: ")
                 continue
+            else:
+                print("zzzz ::::::::::::::::::::::::::::: ")
 
             doc = {}
             for k in log:
