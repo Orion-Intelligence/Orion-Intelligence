1ca420c76d5ccdf8b9c30a4cbaf97ac086e4ff4a
عبدالمنان <msmannan00@protonmail.com>
2025-12-23 22:20:14 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -143,7 +143,6 @@ if [ "$COMMAND" = "build" ] && [ "$FLAG" = "-p" ]; then
     wait_for_server
 fi
 
-
 if [ "$COMMAND" = "build" ] && [ "$FLAG" = "-t" ]; then
     wait_for_test_service
 #    run_test_task
