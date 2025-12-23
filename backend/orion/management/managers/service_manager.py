30c0d63b17dfac4f83306d85ae252822c0785734
عبدالمنان <msmannan00@protonmail.com>
2025-12-23 15:15:00 +0500

    Update service_manager.py

--- backend/orion/management/managers/service_manager.py
+++ backend/orion/management/managers/service_manager.py
@@ -4,13 +4,17 @@ from asyncio import sleep
 from jinja2 import Environment, FileSystemLoader
 
 from orion.api.server.config_manager.config_controller import config_controller
+from orion.helper_manager.env_handler import env_handler
 from orion.management.managers.cronjob_manager import cronjob_manager
 from orion.services.arango_manager.arango_controller import arango_controller
 from orion.services.elastic_manager.elastic_controller import elastic_controller
 from orion.services.mongo_manager.mongo_controller import mongo_controller
+from orion.services.mongo_manager.mongo_enums import MONGO_CONNECTIONS
+from orion.services.mongo_manager.shared_model.db_auth_models import db_user_account
 from orion.services.redis_manager.redis_controller import redis_controller
 from orion.constants.constant import allowed_keys
 from orion.constants import constant
+from orion.services.session_manager.session_enums import admin_mock, admin_user, crawler_mock, crawler_user
 
 
 class service_manager:
@@ -30,7 +34,18 @@ class service_manager:
         self.__url = url
         self._is_available = False
 
+    async def test_server(self):
+        if env_handler.get_instance().env("TESTING_ENABLED", "0") == "1":
+            MONGO_CONNECTIONS.S_MONGO_DATABASE_NAME = 'orion-web_test_v3'
+
+            admin_mock["username"] = "admin_test_username"
+            admin_user["password"] = db_user_account.hash_password("Zq9M#rX@e7W^B0T+f(ysG!kJc1d2mC&N%hAUEP)6Y4n$R8VbHS")
+
+            crawler_mock["username"] = "crawler_test_username"
+            crawler_user["password"] = db_user_account.hash_password("Zq9M#rX@e7W^B0T+f(ysG!kJc1d2mC&N%hAUEP)6Y4n$R8VbHS")
+
     async def init_services(self):
+        await self.test_server()
         while not self._is_available:
             try:
                 _, writer = await asyncio.open_connection("elasticsearch", 9400)
