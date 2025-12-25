b131e78877e73454d635174bad36bba951a785c6
عبدالمنان <msmannan00@protonmail.com>
2025-12-25 22:46:06 +0500

    Update auth-guard.guard.ts

--- client/src/app/shared/guards/auth-guard.guard.ts
+++ client/src/app/shared/guards/auth-guard.guard.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../../services/authetication/auth.service';
 import { AppService } from '../../services/core/app/app.service';
-import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router, private appService: AppService) {
-  }
+  constructor(private authService: AuthService, private router: Router, private appService: AppService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.authService.getIsMobileDemo() && state.url !== '/dashboard/strategic/all') {
+      this.router.navigate(['/dashboard/strategic/all']).then();
+      return false;
+    }
+
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/login'], { queryParams: { redirect: state.url } }).then();
       return false;
     }
-
 
     return true;
   }
