40a6ba561ea27cee33dabfd0ab774a31b7bb49b2
MUHAMMADALI706 <105616718+MUHAMMADALI706@users.noreply.github.com>
2026-03-02 15:25:09 +0500

    fixes

--- client/src/app/pages/credentials/credential.component.ts
+++ client/src/app/pages/credentials/credential.component.ts
@@ -98,6 +98,7 @@ export class CredentialComponent implements OnInit, AfterViewInit {
           this.fetchRanked();
         }
       });
+      this.dashboardService.consolidatedParamModel.q='';
   }
 
   ngAfterViewInit(): void { }
