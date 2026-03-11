d6e4304e6ad147ac280d0f4c451744556c8d7c6c
عبدالمنان <msmannan00@protonmail.com>
2026-03-12 03:04:29 +0500

    fixes

--- client/cypress/e2e/controllers/12-filter-management.controller.ts
+++ client/cypress/e2e/controllers/12-filter-management.controller.ts
@@ -10,14 +10,11 @@ export function selectDateRangeAndReopen() {
   cy.get('[data-testid="side-filter-date-day-1"]').filter(':visible').first().scrollIntoView().click();
   cy.get('[data-testid="side-filter-date-day-25"]').filter(':visible').first().scrollIntoView().click();
   cy.get('[data-testid="side-filter-apply"]').scrollIntoView().click();
-  cy.openSideFilter();
 }
 
 export function selectDateRangeResetAndReopen() {
   cy.get('[data-testid="side-filter-date-toggle"]').scrollIntoView().click();
   cy.get('[data-testid="side-filter-date-day-1"]').filter(':visible').first().scrollIntoView().click();
   cy.get('[data-testid="side-filter-date-day-25"]').filter(':visible').first().scrollIntoView().click();
-  cy.get('[data-testid="side-filter-apply"]').scrollIntoView().click();
   cy.get('[data-testid="side-filter-reset"]').scrollIntoView().click();
-  cy.openSideFilter();
 }
