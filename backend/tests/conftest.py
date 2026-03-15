80a0c6fcd3a5c9ff8982b7449a24ae30c247b4ff
عبدالمنان <msmannan00@protonmail.com>
2026-03-15 14:57:00 +0500

    minor fix

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import asyncio
 import json
 import logging
-import os
 import re
 import shutil
 import sys
@@ -41,26 +40,6 @@ warnings.filterwarnings(
     category=DeprecationWarning,
 )
 logging.getLogger("passlib.handlers.bcrypt").setLevel(logging.ERROR)
-
-
-@pytest.fixture(scope="session", autouse=True)
-def _test_mailpit_host_override():
-    original_host = os.environ.get("ACCOUNTS_SMTP_SERVER")
-    original_port = os.environ.get("ACCOUNTS_SMTP_PORT")
-    os.environ["ACCOUNTS_SMTP_SERVER"] = "localhost"
-    os.environ["ACCOUNTS_SMTP_PORT"] = "1025"
-    try:
-        yield
-    finally:
-        if original_host is None:
-            os.environ.pop("ACCOUNTS_SMTP_SERVER", None)
-        else:
-            os.environ["ACCOUNTS_SMTP_SERVER"] = original_host
-
-        if original_port is None:
-            os.environ.pop("ACCOUNTS_SMTP_PORT", None)
-        else:
-            os.environ["ACCOUNTS_SMTP_PORT"] = original_port
 
 
 def pytest_configure(config):
