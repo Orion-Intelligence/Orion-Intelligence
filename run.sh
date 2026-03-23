0f35a79579a737edc7a96ebce8b1ee5ba0afeea2
Abdul Mannan <msmannan00@protonmail.com>
2026-03-23 20:54:03 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -170,7 +170,7 @@ else
 fi
 
 docker network create --driver bridge shared_bridge 2>/dev/null || true
-docker compose -p "$PROJECT_NAME" -f "$COMPOSE_FILE" up
+docker compose -p "$PROJECT_NAME" -f "$COMPOSE_FILE" up -d
 
 if [ "$COMMAND" = "build" ] && [ "$FLAG" = "-p" ]; then
     wait_for_server
