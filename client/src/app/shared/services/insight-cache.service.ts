5eeb722dbd5679c3b2d3f165745aa1c1833f9e1f
Abdul Mannan <msmannan00@protonmail.com>
2026-04-01 05:16:30 +0500

    minor fixes

--- client/src/app/shared/services/insight-cache.service.ts
+++ client/src/app/shared/services/insight-cache.service.ts
@@ -6,6 +6,7 @@ import { ApiService } from './api.service';
 @Injectable({ providedIn: 'root' })
 export class InsightCacheService {
   private insight$?: Observable<any>;
+  private warmed = false;
 
   constructor(private apiService: ApiService) {}
 
@@ -14,5 +15,17 @@ export class InsightCacheService {
       this.insight$ = this.apiService.get<any>('insight').pipe(shareReplay(1));
     }
     return this.insight$;
+  }
+
+  warmInsight(): void {
+    if (this.warmed) {
+      return;
+    }
+    this.warmed = true;
+    this.getInsight().subscribe({
+      error: () => {
+        this.warmed = false;
+      }
+    });
   }
 }
