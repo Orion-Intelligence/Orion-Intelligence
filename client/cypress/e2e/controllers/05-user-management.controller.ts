e838f8ad8a103e954fd53338c8148a46a908fe57
Abdul Mannan <msmannan00@protonmail.com>
2026-04-02 18:28:07 +0500

    minor fixes

--- client/cypress/e2e/controllers/05-user-management.controller.ts
+++ client/cypress/e2e/controllers/05-user-management.controller.ts
@@ -77,12 +77,14 @@ export function addUser(user: ManagedUser) {
 }
 
 export function loginAsUser(username: string, password: string) {
-  cy.clearCookies();
-  cy.clearLocalStorage();
+  cy.intercept('POST', '**/api/token').as('loginRequest');
   cy.visit('/login');
   cy.get('[data-testid="login-user"]').should('be.visible').clear().type(username);
   cy.get('[data-testid="login-pass"]').should('be.visible').clear().type(password, {log: false});
   cy.get('[data-testid="login-button"], input.login-button').first().should('be.visible').click();
+  cy.wait('@loginRequest').then((interception) => {
+    expect(interception.response?.statusCode).to.eq(200);
+  });
   cy.get('[data-testid="profile-menu"], [data-testid="dashboard-main"], [data-testid="dashboard-container"], .dashboard_container')
     .filter(':visible')
     .should('have.length.greaterThan', 0);
