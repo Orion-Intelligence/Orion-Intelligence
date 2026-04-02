6cd3e89fc27221fce3145787a2c46ef65f396ae5
Abdul Mannan <msmannan00@protonmail.com>
2026-04-02 20:55:47 +0500

    minor fix

--- client/post-build.js
+++ client/post-build.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const sourceDir = path.resolve(__dirname, 'build');
 const targetDir = path.resolve(__dirname, '../backend/build');
