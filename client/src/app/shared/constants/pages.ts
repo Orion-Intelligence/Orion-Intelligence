e2593973054b4bac0361774a2176d05b0fe35df9
Abdul Mannan <msmannan00@protonmail.com>
2026-04-05 04:51:15 +0500

    Update pages.ts

--- client/src/app/shared/constants/pages.ts
+++ client/src/app/shared/constants/pages.ts
@@ -19,6 +19,7 @@ export enum Category {
 export enum GeneralSubCategory {
     ALL = 'All',
     GENERAL = 'General',
+    SWARM = 'Swarm',
     FORUMS = 'Forums',
     NEWS = 'News',
     STOLEN = 'Stolen',
