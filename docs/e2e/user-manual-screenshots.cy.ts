aa8e43071277e421110839515dbc0ae1a33f9d45
Abdul Mannan <msmannan00@protonmail.com>
2026-05-04 20:19:09 +0500

    minor fix

--- docs/e2e/user-manual-screenshots.cy.ts
+++ docs/e2e/user-manual-screenshots.cy.ts
@@ -257,7 +257,7 @@ describe('User Manual Screenshot Flow', () => {
     cy.get('[data-testid="heatmap-report-close"]').click();
     cy.get('[data-testid="homepage-search-input"]').click().type('orion');
     capture('homepage-searchbar');
-    cy.get('[data-testid="homepage-search-input"]').clear();
+    cy.get('[data-testid="homepage-search-input"]').filter(':visible').first().type('{selectall}{backspace}', { force: true });
 
     cy.get('[data-testid="profile-menu"]').filter(':visible').first().should('be.visible').click({ scrollBehavior: false });
     cy.get('[data-testid="profile-help-support"]').filter(':visible').first().should('be.visible').click({ scrollBehavior: false });
