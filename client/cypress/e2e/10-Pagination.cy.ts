9abc3ae6fa60705a293d89bb2f65f9760514c8be
Abdul Mannan <msmannan00@protonmail.com>
2026-05-08 13:18:27 +0500

    Update 10-Pagination.cy.ts

--- client/cypress/e2e/10-Pagination.cy.ts
+++ client/cypress/e2e/10-Pagination.cy.ts
@@ -55,7 +55,7 @@ describe('Data Breach – Tabs & Pagination', () => {
       cy.get('tr[id^="item-"], [data-testid="result-card"]').should('exist');
       cy.get('[data-testid="pagination-root"]').should('exist');
 
-      cy.get('[data-testid="pagination-root"]').scrollIntoView().should('be.visible');
+      cy.get('[data-testid="pagination-root"]').scrollIntoView();
       cy.get('[data-testid="pagination-next"]').filter(':visible').scrollIntoView().not(':disabled').then(($btn) => {
           if ($btn.length) {
             cy.wrap($btn).click();
