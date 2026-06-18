e42ef676b692a5d0a58866e832d2756c47e34cc3
Abdul Mannan <msmannan00@protonmail.com>
2026-06-18 20:37:42 +0500

    Update sitecustomize.py

--- backend/sitecustomize.py
+++ backend/sitecustomize.py
@@ -1,3 +1,11 @@
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message=r"'HTTP_422_UNPROCESSABLE_ENTITY' is deprecated\..*",
+    module=r"starlette_admin(\.|$)",
+)
+
 import coverage
 
 coverage.process_startup()
