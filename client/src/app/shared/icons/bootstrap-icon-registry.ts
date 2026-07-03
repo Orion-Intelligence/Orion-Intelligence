e86606a9908cd7e2e1481d087a42201f8c49a242
Abdul Mannan <msmannan00@protonmail.com>
2026-07-04 00:03:10 +0500

    fixes

--- client/src/app/shared/icons/bootstrap-icon-registry.ts
+++ client/src/app/shared/icons/bootstrap-icon-registry.ts
@@ -25,6 +25,7 @@ export type BootstrapIconName =
   | 'bi-check-lg'
   | 'bi-check2-circle'
   | 'bi-chevron-down'
+  | 'bi-chevron-left'
   | 'bi-chevron-right'
   | 'bi-chevron-up'
   | 'bi-circle'
@@ -173,6 +174,7 @@ export const bootstrapIconRegistry: Record<BootstrapIconName, BootstrapIconDefin
   'bi-check2-circle': { viewBox: '0 0 16 16', markup: `<path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
   <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>` },
   'bi-chevron-down': { viewBox: '0 0 16 16', markup: `<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>` },
+  'bi-chevron-left': { viewBox: '0 0 16 16', markup: `<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>` },
   'bi-chevron-right': { viewBox: '0 0 16 16', markup: `<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>` },
   'bi-chevron-up': { viewBox: '0 0 16 16', markup: `<path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>` },
   'bi-circle': { viewBox: '0 0 16 16', markup: `<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>` },
